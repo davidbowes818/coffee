344d87f15b6cb1e45331ee640b595ad85833cd1e
davidbowes818 <davidrb2017@outlook.com>
2021-11-05 15:26:30 +0000

    Created navbar and layout component

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import React from "react"
+import navbar from "../scss/_navbar.scss"
+
+export default function Navbar() {
+  return (
+    <nav id="main-nav" className="py-1">
+      <div className="container">
+        <ul>
+          <li>
+            <Link to={"/"} style={navbar} className="current">
+              Resteraunt
+            </Link>
+          </li>
+          <li>
+            <Link to={"/menu"}>Menu</Link>
+          </li>
+          <StaticImage src="../img/coffee.png" />
+          <li>
+            <Link to={"/gallery"}>Image gallery</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
+import main from "../scss/main.scss"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
-  return <div>Hello</div>
+  return <Navbar />
 }
