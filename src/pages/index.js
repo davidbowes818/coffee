4c5415adec85f71e5c337901bfbffeab4b08f588
davidbowes818 <davidrb2017@outlook.com>
2021-11-10 20:11:10 +0000

    SCSS library initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,27 @@
+import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import main from "../scss/main.scss"
-import Navbar from "../components/Navbar"
 
 export default function Home() {
-  return <Navbar />
+  return (
+    <div>
+      <nav id="main-nav">
+        <ul>
+          <li>
+            <Link to={"/"}>Resteraunt</Link>
+          </li>
+          <li>
+            <Link to={"/menu"}>Menu</Link>
+          </li>
+          <StaticImage src="../img/coffee.png" />
+          <li>
+            <Link to={"/image-gallery"}>Image Gallery</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
